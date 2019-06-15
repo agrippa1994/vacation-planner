f5bfd164eaf2819e5176e2366e941524edbc3626
agrippa1994 <agrippa1994@icloud.com>
2019-06-15 15:56:02 +0200

    initialize db lazily

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const util = require("util");
 const app = express();
 const expressWs = require("express-ws")(app);
@@ -9,22 +10,30 @@ const { Controller } = require("./controller");
 const { NotesController } = require("./notes-controller");
 const { NotesDao } = require("./notes-dao");
 
-// create DAO objects (data access object)
-const db = new sqlite3.Database("./db.sqlite3");
+async function bootstrap() {
+    const db = new sqlite3.Database("./db.sqlite3");
+    // promisify DB
+    db.allAsync = util.promisify(db.all);
+    db.runAsync = util.promisify(db.run);
 
-// promisify DB
-db.allAsync = util.promisify(db.all);
+    // create DAO objects (data access object)
+    const notesDao = new NotesDao(db);
 
-const notesDao = new NotesDao(db);
+    // initalize DAOs (creating tables and so on)
+    await notesDao.init();
 
-// create controllers
-const ctrl = new Controller(app);
-const notesController = new NotesController(notesDao);
+    // create controllers
+    const ctrl = new Controller(app);
+    const notesController = new NotesController(notesDao);
 
-// connect routes with controller methods
-const apiRouter = express.Router();
-apiRouter.get("/notes", notesController.getAllNotes.bind(notesController));
+    // connect routes with controller methods
+    const apiRouter = express.Router();
+    apiRouter.get("/notes", notesController.getAllNotes.bind(notesController));
 
-// mount routers and start web server
-app.use("/api", apiRouter);
-app.listen(3000);
+    // mount routers and start web server
+    app.use(bodyParser.json());
+    app.use("/api", apiRouter);
+    app.listen(3000);
+}
+
+bootstrap();
--- server/notes-dao.js
+++ server/notes-dao.js
@@ -4,8 +4,31 @@ class NotesDao {
         this.db = db;
     }
 
+    async init() {
+        await this.db.runAsync(`
+            CREATE TABLE IF NOT EXISTS note
+            (
+                id ROWID,
+                author TEXT,
+                note TEXT,
+                completed BOOL
+            )
+        `);
+    }
+
     async getAllNotes() {
-        return await this.db.allAsync("SELECT * FROM note");
+        return await this.db.allAsync(`
+            SELECT
+                *
+            FROM note
+        `);
+    }
+
+    async addNote(autor, note, completed) {
+        return await this.db.allAsync(`
+            INSERT INTO
+                note(author, note, completed)
+            VALUES (?, ?, ?)`, [author, note, completed]);
     }
 }
 
