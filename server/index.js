02e276246da1b21ab0f5ead93137c15ec601e4cf
agrippa1994 <agrippa1994@icloud.com>
2019-06-16 20:12:44 +0200

    add cors support

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const util = require("util");
 const app = express();
 const expressWs = require("express-ws")(app);
 const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
 
 const { Controller } = require("./controller");
 const { NotesController } = require("./notes-controller");
@@ -12,6 +13,7 @@ const { NotesDao } = require("./notes-dao");
 
 async function bootstrap() {
     const db = new sqlite3.Database("./db.sqlite3");
+
     // promisify DB
     db.allAsync = util.promisify(db.all);
     db.runAsync = util.promisify(db.run);
@@ -31,6 +33,7 @@ async function bootstrap() {
     apiRouter.get("/notes", notesController.getAllNotes.bind(notesController));
 
     // mount routers and start web server
+    app.use(cors());
     app.use(bodyParser.json());
     app.use("/api", apiRouter);
     app.listen(3000);
