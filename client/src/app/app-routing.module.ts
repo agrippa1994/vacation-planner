4160f04595d065067fc56a93fbfbcac4ca13c6c2
agrippa1994 <agrippa1994@icloud.com>
2019-06-16 21:03:16 +0200

    try to fix loadChildren issue

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,33 +1,10 @@
-import { NgModule, Injectable } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { constructor } from 'q';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes, Router } from '@angular/router';
 import { SettingsService } from './settings.service';
 
-@Injectable({
-  providedIn: "root"
-})
-class CanActivateApp implements CanActivate {
-
-  constructor(
-    private router: Router,
-    private settingsService: SettingsService,
-  ) {}
-
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
-    Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
-
-    if(this.settingsService.areSettingsValid)
-      return true;
-
-    this.router.navigate(["settings"]);
-    return false;
-  }
-
-}
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [CanActivateApp] },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' }
 ];
 @NgModule({
@@ -37,7 +14,14 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-  constructor() {
+  constructor(
+    private router: Router,
+    private settingsService: SettingsService,
+  ) {
+    if(this.settingsService.areSettingsValid)
+      return;
 
+    this.router.navigate(["settings"]);
+    return;
   }
 }
