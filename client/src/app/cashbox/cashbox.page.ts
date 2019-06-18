5c49229c7ece9510ff6d9e87b8a89dc31fdad07c
Agnieszka <agnieszkaasowinska@gmail.com>
2019-06-18 19:51:26 +0200

    cashbox basic features

--- client/src/app/cashbox/cashbox.page.ts
+++ client/src/app/cashbox/cashbox.page.ts
@@ -7,10 +7,46 @@ import { CashboxService } from '../cashbox.service';
   styleUrls: ['./cashbox.page.scss'],
 })
 export class CashboxPage implements OnInit {
+    expenses = [];
+    expenseAmount = 0;
+    total = [];
+    addExpenseReason(newExpenseReason: string) {
+        if (newExpenseReason) {
+            this.expenses.push(newExpenseReason);
+        }
+    }
+
+    clear(newExpenseReason: string) {
+        if (newExpenseReason) {
+            this.expenses.pop(newExpenseReason);
+        }
+    }
+
+
+    /*totalExpense(newExpenseAmount: string) {
+        if (newExpenseAmount) {
+            this.total.push(newExpenseAmount);
+            /!*this.total = +((this.expenseAmount) + (this.expenseAmount));*!/
+        }
+        }*/
+
+
+
+    /*total: number;
+    totalExpense(old:number, new:number) {
+        this.total = +old + +new;
+
+    }*/
+    /*totalExpense(newExpenseAmount: string ) {
+        if (newExpenseAmount) {
+          this.expenses += + newExpenseAmount;
+        }
+    }*/
 
   constructor(private cashboxService: CashboxService) { }
 
   ngOnInit() {
   }
+
 
 }
