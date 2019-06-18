0b9e2a50aad4aebe9b54b87caa6eb3b58765071b
agrippa1994 <agrippa1994@icloud.com>
2019-06-18 18:38:58 +0200

    add notes backend

--- server/notes-dao.js
+++ server/notes-dao.js
@@ -8,10 +8,10 @@ class NotesDao {
         await this.db.runAsync(`
             CREATE TABLE IF NOT EXISTS note
             (
-                id ROWID,
-                author TEXT,
-                note TEXT,
-                completed BOOL
+                id INTEGER PRIMARY KEY,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                username TEXT,
+                note TEXT
             )
         `);
     }
@@ -24,12 +24,32 @@ class NotesDao {
         `);
     }
 
-    async addNote(autor, note, completed) {
+    async addNote(username, note) {
         return await this.db.allAsync(`
             INSERT INTO
-                note(author, note, completed)
-            VALUES (?, ?, ?)`, [author, note, completed]);
+                note(id, username, note)
+            VALUES (NULL, ?, ?)`, [username, note]);
     }
+
+    async deleteNote(id) {
+        return await this.db.allAsync(`
+            DELETE FROM note
+            WHERE id = ?`, [id]);
+    }
+
+    async updateNote(id, username, note) {
+        return await this.db.allAsync(`
+            UPDATE
+                note
+            SET
+                username = ?,
+                note = ?
+            WHERE
+                id = ?
+            `, [username, note, id]);
+    }
+
+
 }
 
 module.exports =  {
