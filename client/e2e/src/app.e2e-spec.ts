777af26d18a2fb761b70b1a70079d6686e012913
agrippa1994 <agrippa1994@icloud.com>
2019-06-19 20:03:02 +0200

    make data-driven test for testing navigation

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,69 +1,50 @@
 import { AppPage } from './app.po';
-import { browser } from 'protractor';
+import { browser, ExpectedConditions } from 'protractor';
 import { SettingsPage } from './settings.po';
 import { settings } from 'cluster';
 import { MapPage } from './map.po';
 import { CashboxPage } from './cashbox.po';
 import { NotesPage } from './notes.po';
 import { OverviewPage } from './overview.po';
+import { TabsPage } from './tabspage';
 
 describe('new App', () => {
   let page: AppPage;
-  let overviewPage: OverviewPage;
-  let settingsPage: SettingsPage;
-  let mapPage: MapPage;
-  let cashboxPage: CashboxPage;
-  let notesPage: NotesPage;
 
   beforeEach(() => {
     page = new AppPage();
-    settingsPage = new SettingsPage();
-    mapPage = new MapPage();
-    cashboxPage = new CashboxPage();
-    notesPage = new NotesPage();
-    overviewPage = new OverviewPage();
-
-    browser.manage().timeouts().implicitlyWait(10000);
-    browser.manage().timeouts().pageLoadTimeout(2000);
-    browser.sleep(1000);
   });
 
 
-  it('should login', async () => {
+  it('should login', () => {
     page.navigateTo();
-    expect(await settingsPage.isShown()).toBeTruthy();
+    const settingsPage = new SettingsPage();
 
-    await settingsPage.enterServerUrl("http://localhost:3000");
-    await settingsPage.enterUsername("automated e2e test user");
-    await settingsPage.clickSave();
+    browser.wait(ExpectedConditions.urlContains(settingsPage.pageUrl));
+
+    settingsPage.enterServerUrl("http://localhost:3000");
+    settingsPage.enterUsername("automated e2e test user");
+    settingsPage.clickSave();
   });
 
-  it("should navigate to overview page", async () => {
-    await page.gotoTab(overviewPage);
-    await expect(await overviewPage.isShown()).toBeTruthy();
+  describe("tab bar navigation", () => {
+
+    [new OverviewPage(), new MapPage(), new CashboxPage(), new NotesPage(), new SettingsPage()].forEach((tab: TabsPage) => {
+
+      it(`should navigate to ${tab.pageUrl} tab`, () => {
+        page.navigateTo();
+        page.gotoTab(tab);
+        browser.wait(ExpectedConditions.urlContains(tab.pageUrl));
+      });
+
+      it(`should not change tab ${tab.pageUrl} if browser reloads page`, () => {
+        browser.wait(ExpectedConditions.urlContains(tab.pageUrl));
+        browser.refresh();
+        browser.wait(ExpectedConditions.urlContains(tab.pageUrl));
+      });
+
+    });
+
   });
 
-  it("should navigate to map page", async () => {
-    await page.gotoTab(mapPage);
-    await expect(await mapPage.isShown()).toBeTruthy();
-  });
-
-  it("should navigate to cashbox page", async () => {
-    await page.gotoTab(cashboxPage);
-    await expect(await cashboxPage.isShown()).toBeTruthy();
-  });
-
-  it("should navigate to notes page", async () => {
-    await page.gotoTab(notesPage);
-    await expect(await notesPage.isShown()).toBeTruthy();
-  });
-
-  it("should navigate to settings page", async () => {
-    await page.gotoTab(settingsPage);
-    await expect(await settingsPage.isShown()).toBeTruthy();
-  });
-
-  it("should do something else", async () => {
-    console.log("hi");
-  });
 });
