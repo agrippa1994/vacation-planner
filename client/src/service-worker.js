92fc7d46b3909919a84369e000952a570dc59c99
wechtig <wechtitsch.lukas@gmail.com>
2019-08-18 20:55:14 +0200

    add pwa structure

---
+++ client/src/service-worker.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+    name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+    [
+        'index.html',
+        'manifest.json'
+    ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;
