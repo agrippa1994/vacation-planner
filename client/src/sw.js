743a13939697efa0a88a024d7420f967917d859e
wechtig <wechtitsch.lukas@gmail.com>
2019-08-20 19:07:04 +0200

    fix pwa bug; add img

--- client/src/sw.js
+++ client/src/sw.js
@@ -24,3 +24,4 @@ self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
